8fead7d5bc905239493670c7abd2b30b98daa94d
Tmdsb941 <1927919399@qq.com>
2019-02-28 21:55:19 +0800

    更新1.0

--- passcloud-cloud/passcloud-cloud-eureka/src/main/java/com/cs/yang/passcloudcloudeureka/PasscloudCloudEurekaApplication.java
+++ passcloud-cloud/passcloud-cloud-eureka/src/main/java/com/cs/yang/passcloudcloudeureka/PasscloudCloudEurekaApplication.java
@@ -2,10 +2,12 @@ package com.cs.yang.passcloudcloudeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EntityScan("com.cs.yang.*.*")
 public class PasscloudCloudEurekaApplication {
 
     public static void main(String[] args) {
